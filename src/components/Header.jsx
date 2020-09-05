65e14aaa0ba7a88715ece2a4fb8467f68a3bc31e
Samer <66347142+wwidprog@users.noreply.github.com>
2020-09-05 18:22:37 -0400

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import OfflineBoltIcon from "@material-ui/icons/OfflineBolt";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <OfflineBoltIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
